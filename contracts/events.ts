4b42e7048aafa896341e5db83d463eedabb83053
Giuseppe <giuseppecv56@gmail.com>
2022-05-19 15:08:03 -0500

    build: SMAR-520 add template basic for adonis framework

---
+++ contracts/events.ts
@@ -0,0 +1,10 @@
+/**
+ * Contract source: https://git.io/JfefG
+ *
+ * Feel free to let us know via PR, if you find something broken in this contract
+ * file.
+ */
+
+declare module '@ioc:Adonis/Core/Event' {
+  interface EventsList {}
+}
