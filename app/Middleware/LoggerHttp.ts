a7b9de0a6060adfb8b0db4ef500760ffae3391e7
Giuseppe <giuseppecv56@gmail.com>
2022-05-19 23:30:54 -0500

    chore: SMAR-521 add date in logger http

--- app/Middleware/LoggerHttp.ts
+++ app/Middleware/LoggerHttp.ts
@@ -1,7 +1,16 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { dim, cyan, green, blue, red, yellow, bold, bgRed } from 'colorette';
-import morgan from 'morgan';
 import Env from '@ioc:Adonis/Core/Env';
+import morgan from 'morgan';
+import {
+  dim,
+  cyanBright,
+  greenBright,
+  blueBright,
+  redBright,
+  yellowBright,
+  bold,
+  bgRed,
+} from 'colorette';
 
 export default class LoggerHttp {
   public async handle(
@@ -10,32 +19,32 @@ export default class LoggerHttp {
   ) {
     const status = (status: string) => {
       if (parseInt(status, 10) >= 500) return bgRed(status);
-      if (parseInt(status, 10) >= 400) return red(status);
-      if (parseInt(status, 10) >= 300) return cyan(status);
-      return green(status);
+      if (parseInt(status, 10) >= 400) return redBright(status);
+      if (parseInt(status, 10) >= 300) return cyanBright(status);
+      return greenBright(status);
     };
 
     const method = (method: string) => {
-      if (method === 'GET') return bold(blue(method));
-      if (method === 'POST') return bold(green(method));
-      if (method === 'PUT') return bold(yellow(method));
-      if (method === 'PATCH') return bold(cyan(method));
-      if (method === 'DELETE') return bold(red(method));
+      if (method === 'GET') return bold(blueBright(method));
+      if (method === 'POST') return bold(greenBright(method));
+      if (method === 'PUT') return bold(yellowBright(method));
+      if (method === 'PATCH') return bold(cyanBright(method));
+      if (method === 'DELETE') return bold(redBright(method));
       return dim(method);
     };
     const loggerHttp = morgan((tokens, req, res) => {
       if (Env.get('NODE_ENV') === 'test') return;
       return [
+        tokens.date(req, res),
         method(tokens.method(req, res) ?? '-'),
         dim(tokens.url(req, res) ?? '-'),
         status(tokens.status(req, res) ?? '-'),
-        tokens.res(req, res, 'content-length'),
+        '-',
+        tokens.res(req, res, 'content-length') || 0,
         '-',
         tokens['response-time'](req, res),
         'ms',
       ].join(' ');
-      /* console.log(req.request);
-      console.log(res.response); */
     });
     loggerHttp(request.request, response.response, () => {});
 
