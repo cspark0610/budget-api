89d400d0c63e44d13e5f2372f811ca586d969be0
Gael Gonzales <gaelinho76@gmail.com>
2022-07-15 14:56:51 -0500

    feat: SMAR-558 Add tokenks fro area

---
+++ src/core/domain/project-area/di/ProjectAreaDITokens.ts
@@ -0,0 +1,12 @@
+export class ProjecAreaDITokens {
+  // Use-cases
+
+  /* public static readonly CreateUserUseCase: unique symbol =
+      Symbol('CreateUserUseCase'); */
+
+  // Repositories
+
+  public static readonly ProjectAreaRepository: unique symbol = Symbol(
+    'ProjectAreaRepository',
+  );
+}
